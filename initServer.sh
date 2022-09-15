87ecf73eacfab988eb973f6302f1f9a04395d7e7
Raghavendra Khare <64489407+raghavxk@users.noreply.github.com>
2022-09-15 23:41:46 +0530

    connected event listener for transactions

--- initServer.sh
+++ initServer.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # handle backend-server dependencies
+node ./server/eventListeners/transactions.js
 printf "started resolving dependencies for backend server \n"
 npm install --prefix ./server
 printf "successfully resolved backend dependencies \n\n"
